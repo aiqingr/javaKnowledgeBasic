b51089d4119c958b81aa6bb0c8145de19ac987f0
Tongyang Ni <ntyaiqingr@gmail.com>
2020-09-23 23:20:58 -0400

    constructor with limit class parameters

--- secionOOP/src/javalearning/Main.java
+++ secionOOP/src/javalearning/Main.java
@@ -25,5 +25,17 @@ public class Main {
 
         nicksAccount.deposit(100);
         nicksAccount.withdrawal(100);
+
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
+
+        VipPerson person2 = new VipPerson("Nick", 20000);
+        System.out.println(person2.getName());
+        System.out.println(person2.getEmailAddress());
+
+        VipPerson person3 = new VipPerson("maoMao", 100000, "123@123.com");
+        System.out.println(person3.getName());
+        System.out.println(person3.getCreditLimit());
+        System.out.println(person3.getEmailAddress());
     }
 }
